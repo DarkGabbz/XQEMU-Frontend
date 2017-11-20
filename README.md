d89601ecfdf79887c6b84e078c4c56cb16d0a4c3
Voxel <thesupersonic67@gmail.com>
2017-11-20 21:07:48 +0000

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# XQEMU_Frontend
+# XQEMU Frontend
+This is a small Windows utility coded in C# which helps ease the process of launching Xbox ISOs/Dashboards through XQEMU. A friend requested this on Discord, so I decided to give it a shot.
+
+This was my first ever attempt at developing a C# application in my life, so forgive me for the slightly unforgiving, messy code! ;P
+
+Usage:
+======
+Very simple usage; just browse for the appropriate files and hit "Start XQEMU". If you would like to view the full BIOS boot animation, tick the "Show Full Boot Animation" checkbox at the bottom to do so.
+- If 'qemu-system-xbox.exe' isn't located in the same directory as the frontend, a file dialog window will appear asking you to locate the XQEMU executable.
+- Before it will allow you to run XQEMU, the frontend requires you to have selected the BIOS, MCPX, and harddisk image. If you wish to boot to the dashboard, leave the ISO textbox empty.
+
+Enjoy!
---
+++ Xqemu-Frontend/MainWindow.xaml.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Security.Permissions;
+using System.Diagnostics;
+
+namespace Xqemu_Frontend
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text == "" || textBox1.Text == "" || textBox3.Text == "")
+            {
+                System.Windows.MessageBox.Show("You must fill in BIOS, MCPX and HDD fields!");
+            } else
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "qemu-system-xbox.exe";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+                // Note: This isn't really the right way to do this... -Voxel
+                if (checkBox.IsChecked == true)
+                {
+                    startInfo.Arguments = "-cpu pentium3 -machine xbox,bootrom=" + textBox1.Text +
+                        " -m 128 -drive file=" + textBox3.Text + ",index=0,media=disk,locked=on -drive file=" + 
+                        textBox2.Text + ",index=1,media=cdrom -bios " + textBox.Text +
+                        " -usb -device usb-hub,bus=usb-bus.0,port=3 -device usb-xbox-gamepad,bus=usb-bus.0,port=3.2";
+                } else
+                {
+                    startInfo.Arguments = "-cpu pentium3 -machine xbox,short_animation,bootrom=" + textBox1.Text +
+                        " -m 128 -drive file=" + textBox3.Text + ",index=0,media=disk,locked=on -drive file=" +
+                        textBox2.Text + ",index=1,media=cdrom -bios " + textBox.Text +
+                        " -usb -device usb-hub,bus=usb-bus.0,port=3 -device usb-xbox-gamepad,bus=usb-bus.0,port=3.2";
+                }
+                
+                try
+                {
+                    using (Process exeProcess = Process.Start(startInfo))
+                    {
+                        exeProcess.WaitForExit();
+                    }
+                }
+                catch
+                {
+                    // Display File Dialog
+                    OpenFileDialog openFileDialog5 = new OpenFileDialog();
+                    openFileDialog5.Filter = "Executable|*.exe";
+                    openFileDialog5.Title = "Please Locate XQEMU Executable";
+
+                    // Show the Dialog
+                    if (openFileDialog5.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        // Append Text Box
+                        startInfo.FileName = openFileDialog5.FileName;
+
+                        try
+                        {
+                            using (Process exeProcess = Process.Start(startInfo))
+                            {
+                                exeProcess.WaitForExit();
+                            }
+                        }
+                        catch
+                        {
+                            // Do absolutely nothing...
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Display File Dialog
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Xbox BIOS File|*.bin";
+            openFileDialog1.Title = "Select an Xbox BIOS";
+
+            // Show the Dialog
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Append Text Box
+                textBox.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            // Display File Dialog
+            OpenFileDialog openFileDialog2 = new OpenFileDialog();
+            openFileDialog2.Filter = "MCPX ROM|*.bin";
+            openFileDialog2.Title = "Select the MCPX ROM";
+
+            // Show the Dialog
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Append Text Box
+                textBox1.Text = openFileDialog2.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            // Display File Dialog
+            OpenFileDialog openFileDialog3 = new OpenFileDialog();
+            openFileDialog3.Filter = "Xbox ISO|*.iso";
+            openFileDialog3.Title = "Select an Xbox ISO";
+
+            // Show the Dialog
+            if (openFileDialog3.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Append Text Box
+                textBox2.Text = openFileDialog3.FileName;
+            }
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            // Display File Dialog
+            OpenFileDialog openFileDialog4 = new OpenFileDialog();
+            openFileDialog4.Filter = "QEMU Harddisk|*.qcow2";
+            openFileDialog4.Title = "Select an Xbox Harddisk";
+
+            // Show the Dialog
+            if (openFileDialog4.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Append Text Box
+                textBox3.Text = openFileDialog4.FileName;
+            }
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
